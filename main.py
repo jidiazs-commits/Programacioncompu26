ea1868f2188548ecbff3a272951cfceece062426
Sebas <sebastianvelace@gmail.com>
2026-04-23 09:24:38 -0500

    comentarios

--- main.py
+++ main.py
@@ -60,19 +60,20 @@ def procesar_estadisticas_vistas(ruta_archivo):
             if es_encabezado == True:
                 es_encabezado = False
                 continue
-            linea = linea.strip() #aqui selimpia la línea de saltos invisibles (\n) antes de cortarla
+            linea = linea.strip() #aqui se limpia la línea de saltos invisibles (\n) antes de cortarla
             # Corta la línea de texto cada vez que encuentra una coma y lo convierte a una lista de palabras
             columnas = linea.split(',')
             # Valido que la fila no este rota o vacia para que no de error
             if len(columnas) < 9:
                 continue
-            nombre_video = columnas[1]#accede a la última columna para los likes
-            vistas_str = columnas[-2]# Accede a la penultima columna
-            likes_str = columnas[-1]
-            #Llama a la función de transformar el texto a un número
+            nombre_video = columnas[1]#extrae el nombre del video que está en la segunda posición
+            vistas_str = columnas[-2]# accede a la penultima columna
+            likes_str = columnas[-1]#accede a la última columna para los likes
+            
+            #Llama a la función de transformar el texto a un número de vistas y likes
             valor_numerico_vistas = convertir(vistas_str)
             valor_numerico_likes = convertir(likes_str)
-            ##Ahi cree la condicion que faltaba para qeu el primer numero es el mas grande y el mas pequeño de los numeros
+            ##Condicion que faltaba para qeu el primer numero es el mas grande y el mas pequeño de los numeros
             if es_primer_dato == True:
                 # Si es el primer dato que se lee, es el mayor y el menor a la vez
                 max_val = valor_numerico_vistas
