c1e69d833803632a06fe3b24a932bba2deb66257
Samuel1983 <samuelcastellanos237@gmail.com>
2026-04-23 09:32:54 -0500

    Variables 1

--- main.py
+++ main.py
@@ -73,11 +73,16 @@ def procesar_estadisticas_vistas(ruta_archivo):
             #Llama a la función de transformar el texto a un número de vistas y likes
             valor_numerico_vistas = convertir(vistas_str)
             valor_numerico_likes = convertir(likes_str)
-            ##Condicion que faltaba para qeu el primer numero es el mas grande y el mas pequeño de los numeros
+            ##Condicion que faltaba para que el primer numero sea el mas grande y el mas pequeño de los numeros
             if es_primer_dato == True:
                 # Si es el primer dato que se lee, es el mayor y el menor a la vez
-                max_val = valor_numerico_vistas
-                min_val = valor_numerico_vistas
+                max_val_vistas = valor_numerico_vistas
+                min_val_vistas = valor_numerico_vistas
+                #Inicia en la primera pasada las variables de likes y de los nombres
+                max_val_likes = valor_numerico_likes
+                min_val_likes = valor_numerico_likes
+                nombre_max_vistas = nombre_video
+
                 es_primer_dato = False ##Esto es porque como la fila tiene los titulos de las columnas pues no se pueden procesar ni nos interesa que se procese
             else:
                 # y sii no es el primero, se compara con los que ya se tienen guardados
